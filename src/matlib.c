9fca0aab31413d688bade77286b64c55173ec389
Ilija Radosavovic <ilija.radosavovic13@imperial.ac.uk>
2015-03-08 23:30:17 +0000

    Added functions for constructing and destroying matrices.

--- src/matlib.c
+++ src/matlib.c
@@ -1,1 +1,87 @@
+#include <assert.h>
+#include <stdio.h>
+
 #include "matlib.h"
+
+struct matrix*
+new_matrix(size_t m, size_t n)
+{
+  assert(m >= 0 && n >= 0);
+
+  /* Allocate memory for the matrix struct. */
+  struct matrix* mat = (struct matrix*) malloc(sizeof(struct matrix));
+  if (mat == NULL)
+  {
+    perror("Failed to allocate memory for mat.");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Set the dimensions. */
+  mat->m = m;
+  mat->n = n;
+
+  /* Allocate memory for the rows. */
+  mat->data = (float**) malloc(sizeof(float*) * m);
+  if (mat->data == NULL)
+  {
+    perror("Failed to allocate memory for mat rows.");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Allocate memory for the columns. */
+  for (size_t i = 0; i < m; i++)
+  {
+    mat->data[i] = (float*) malloc(sizeof(float) * n); 
+    if (mat->data[i] == NULL)
+    {
+      perror("Failed to allocate memory for mat columns");
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  return mat;
+}
+
+void
+destroy_matrix(struct matrix* mat)
+{
+  assert(mat != NULL);
+
+  /* Free the columns. */
+  for (size_t i = 0; i < mat->m; i++)
+  {
+    free(mat->data[i]);
+  }
+
+  /* Free the rows. */
+  free(mat->data);
+  /* Free the matrix struct. */
+  free(mat);
+}
+
+struct vector*
+new_vector(size_t n)
+{
+  assert(n >= 0);
+  
+  /* Allocate memory for the vector struct. */
+  struct vector* v = (struct vector*) malloc(sizeof(struct vector));
+  if (v == NULL)
+  {
+    perror("Failed to allocate memory for the vector struct.");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Set the size. */
+  v->n = n;
+
+  /* Allocate memory for data. */
+  v->data = (float*) malloc(sizeof(float) * n);
+  if (v->data == NULL)
+  {
+    perror("Failed to allocate memory for data.");
+    exit(EXIT_FAILURE);
+  }
+
+  return v;
+}
--- src/matlib.h
+++ src/matlib.h
@@ -1,5 +1,33 @@
 #ifndef MATLIB_H
 #define MATLIB_H
 
+#include <stdlib.h>
+
+/* Matrix & vector library. */
+
+/* Matrix representation. */
+struct matrix 
+{
+  size_t m;      /* Number of rows. */
+  size_t n;      /* Number of columns. */
+  float** data;  /* Elements. */
+};
+
+/* Vector representation. */
+struct vector
+{
+  size_t n;     /* Dimension. */
+  float* data;  /* Elements. */
+};
+
+/* Allocates matrix of size m x n on the heap. */
+struct matrix* new_matrix(size_t m, size_t n);
+/* Frees the memory used by the matrix mat. */
+void destroy_matrix(struct matrix* mat);
+
+/* Allocates vector of size n. */
+struct vector* new_vector(size_t n);
+/* Frees the memory used by the vector v. */
+void destroy_vector(struct vector* v);
 
 #endif /* src/matlib.h */
